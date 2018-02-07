c0e7814c0d9460d17992cb26fae005cdd5fe193a
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2018-02-07 13:02:12 +0200

    Added some 10x10 matrices for tests
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+1) Install the MKL bindings:
 Compute.Net bindings for MKL, Cuda, etc
 Project site: https://github.com/allisterb/Compute.NET
 Needs .Net core 2.0
@@ -5,3 +6,6 @@ Usage (see site for current instructions):
     1) Add the Compute.NET package feed to your NuGet package sources: https://www.myget.org/F/computedotnet/api/v2
     2) Install the bindings package into your project: Install-Package Compute.Bindings.IntelMKL.
     3) (Optional) Install the native library package into your project: Install-Package Compute.Winx64.IntelMKL.
+
+2) After installing these packages and the dlls:
+go to project -> Properties -> Build and set Platform target from "Any CPU" to "x64" 
---
+++ TestMKL/Utilities.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKLtest
+{
+    static class Utilities
+    {
+        public static double[] Round(double[] array, int decimals)
+        {
+            double[] rounded = new double[array.Length];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                rounded[i] = Math.Round(array[i], decimals);
+            }
+            return rounded;
+        }
+
+        public static double[,] Round(double[,] array, int decimals)
+        {
+            double[,] rounded = new double[array.GetLength(0), array.GetLength(1)];
+            for (int i = 0; i < array.GetLength(0); ++i)
+            {
+                for (int j = 0; j < array.GetLength(1); ++j)
+                {
+                    rounded[i, j] = Math.Round(array[i, j], decimals);
+                }
+            }
+            return rounded;
+        }
+    }
+}
